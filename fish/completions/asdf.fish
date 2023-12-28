cf91dd291d40b1f8497a34f4334a430100b20d4c
Rob Loach <robloach@gmail.com>
2023-12-28 00:51:49 -0500

    Remove firsh

--- fish/completions/asdf.fish
+++
@@ -1,1 +0,0 @@
-/home/rob/.asdf/completions/asdf.fish
--- fish/config.fish
+++
@@ -1,8 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-# asdf-vm
-source ~/.asdf/asdf.fish
-
-fish_add_path ~/.local/bin
