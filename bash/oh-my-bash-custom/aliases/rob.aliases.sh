3d6a23bf538085a00227c388f3f34afaa1915038
Rob Loach <robloach@gmail.com>
2024-09-01 12:42:39 -0400

    Update

--- bash/oh-my-bash-custom/aliases/rob.aliases.sh
+++ bash/oh-my-bash-custom/aliases/rob.aliases.sh
@@ -5,11 +5,9 @@ alias gti='git'
 alias gt='git'
 alias gi='git'
 alias maek='make'
-alias dotfiles='make -C ~/.dotfiles'
+alias dotfiles='make -C ${DOTFILES}'
 
 # Path
-export PATH="$PATH:$HOME/.bin"
-export PATH="$HOME/.local/bin:$PATH";
+export PATH="$PATH:$HOME/.bin:$HOME/.local/bin";
 export PATH="$HOME/Documents/RetroArch:$PATH";
 export PATH="$HOME/.cargo/bin:$PATH";
-export PATH="$HOME/.lando/bin:$PATH"; #landopath
