1d81f67078a9bf6fcf7c5d196b4de4c2ebaaf047
Rob Loach <robloach@gmail.com>
2024-09-04 13:38:48 -0400

    Add more aliases

--- .oh-my-bash-custom/aliases/rob.aliases.sh
+++ .oh-my-bash-custom/aliases/rob.aliases.sh
@@ -1,11 +1,22 @@
 #! bash oh-my-bash.module
 
-# Aliases
+# git
 alias gti='git'
 alias gt='git'
 alias gi='git'
+
+# make
 alias maek='make'
+
+# dotfiles
 alias dotfiles='make -C ${DOTFILES}'
+
+# composer
+alias composre='composer'
+alias xomposer='composer'
+alias cimposer='composer'
+alias clmposer='composer'
+alias cpmposer='composer'
 
 # Path
 export PATH="$PATH:$HOME/.bin:$HOME/.local/bin";
