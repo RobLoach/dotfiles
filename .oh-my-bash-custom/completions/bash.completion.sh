5aead7e70756819c9e69992351d0b40d28b42b2c
Rob Loach <robloach@gmail.com>
2024-09-05 12:51:18 -0400

    Add bash_completion

---
+++ .oh-my-bash-custom/completions/bash.completion.sh
@@ -0,0 +1,27 @@
+#! bash oh-my-bash.module
+#
+# bash-completion
+# https://github.com/scop/bash-completion
+
+if [[ $PS1 ]]; then
+	# Ensure the `have` command is available
+	have()
+	{
+		unset -v have
+		_comp_have_command "$1" && have=yes
+	}
+
+	# Custom BASH_COMPLETION_DIR
+	if [[ ${BASH_COMPLETION_DIR+set} ]]; then
+		. ${BASH_COMPLETION_DIR}/bash_completion
+	# Config directory
+	elif [[ -f "${XDG_CONFIG_HOME:-$HOME/.config}/bash_completion/bash_completion" ]]; then
+		. ${XDG_CONFIG_HOME:-$HOME/.config}/bash_completion/bash_completion
+	# /usr/share/bash-completion
+	elif [[ -f /usr/share/bash-completion/bash_completion ]]; then
+		. /usr/share/bash-completion/bash_completion
+	# /sw/etc/bash_completion
+	elif [[ -f /sw/etc/bash_completion/bash_completion ]]; then
+		. /sw/etc/bash_completion/bash_completion
+	fi
+fi
