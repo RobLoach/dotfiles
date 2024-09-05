90f80b8bdf7234a662c1ecd9f251a84cf6c16b08
Rob Loach <robloach@gmail.com>
2024-09-05 13:19:28 -0400

    Use omb

--- .oh-my-bash-custom/completions/bash.completion.sh
+++ .oh-my-bash-custom/completions/bash.completion.sh
@@ -4,11 +4,11 @@
 # https://github.com/scop/bash-completion
 
 if [[ $PS1 ]]; then
-	# Ensure the `have` command is available
+	# Use _omb_util_command_exists() for bash_completion's have() command
 	have()
 	{
 		unset -v have
-		_comp_have_command "$1" && have=yes
+		_omb_util_command_exists "$1"
 	}
 
 	# Custom BASH_COMPLETION_DIR
@@ -24,4 +24,7 @@ if [[ $PS1 ]]; then
 	elif [[ -f /sw/etc/bash_completion/bash_completion ]]; then
 		. /sw/etc/bash_completion/bash_completion
 	fi
+
+	# The have() command is no longer needed
+	unset -v have
 fi
